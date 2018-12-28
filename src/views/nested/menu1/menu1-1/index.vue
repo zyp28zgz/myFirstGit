a47996f821d15625a92db2a0e90c3880d185ef58
zyp28zgz <1184193408@qq.com>
2018-12-28 15:40:59 +0800

    修改内容4

--- src/views/nested/menu1/menu1-1/index.vue
+++ src/views/nested/menu1/menu1-1/index.vue
@@ -2,7 +2,7 @@
   <div style="padding:30px;">
     <el-alert :closable="false" title="menu 1-1" type="success">
       <router-view />
-      dssdsdsdhello ,welcome my roomyyppppppppyyyy;
+      dssdsdsdhello ,welcome my roomyyppppppppyyyyeeeee;
     </el-alert>
   </div>
 </template>
